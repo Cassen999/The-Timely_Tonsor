3d3d04d728665385398e220294460fe1d379c9b4
Cassen Gerber <cassenpt@gmail.com>
2021-01-12 11:01:15 -0600

    created the UserLandingPage, adjusted App.js to show UserLandingPage instead of UserPage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,7 @@ import Footer from '../Footer/Footer';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 
 import AboutPage from '../AboutPage/AboutPage';
-import UserPage from '../UserPage/UserPage';
+import UserLandingPage from '../UserLandingPage/UserLandingPage';
 import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
@@ -52,7 +52,7 @@ class App extends Component {
               // logged in shows UserPage else shows LoginPage
               exact
               path="/user"
-              component={UserPage}
+              component={UserLandingPage}
             />
 
             <ProtectedRoute
